a68207a210bedd81f1d37bf36387b7d54d68ca62
Sunil Pandey <sunilcid760@gmail.com>
2022-04-08 20:13:48 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SecondDBEntities context;
+
+        public HomeController()
+        {
+            context= new SecondDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ShowData()
+        {
+            List<tbl_Employee> emp = new List<tbl_Employee>();
+            emp = context.tbl_Employee.ToList();
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Create(tbl_Employee model)
+        {
+            var emp = new tbl_Employee()
+            {
+                FirstName=model.FirstName,
+                LastName=model.LastName,
+                Gender=model.Gender,
+                Mobile=model.Mobile,
+                Email=model.Email
+            };
+            context.tbl_Employee.Add(emp);
+            context.SaveChanges();
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var emp = context.tbl_Employee.SingleOrDefault(e => e.Id == id);
+            context.tbl_Employee.Remove(emp);
+            context.SaveChanges();
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+       
+       
+        [HttpGet]
+        public JsonResult Edit(int id)
+        {
+            var emps = context.tbl_Employee.SingleOrDefault(e => e.Id == id);
+            return Json(emps, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Update(tbl_Employee model)
+        {
+            var emp = new tbl_Employee()
+            {
+                Id=model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Gender = model.Gender,
+                Mobile = model.Mobile,
+                Email = model.Email
+            };
+            context.Entry(emp).State = System.Data.EntityState.Modified;
+            context.SaveChanges();
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+       
+    }
+}
---
+++ Scripts/javaScript/Custom.js
@@ -0,0 +1,129 @@
+﻿
+
+$(document).ready(function () {
+
+    ShowDataAll();
+
+});
+function ShowDataAll() {
+    $.ajax({
+        url: '/Home/ShowData',
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8',
+        success: function (result) {
+            var obj = '';
+            $.each(result, function (key, item) {
+                obj += '<tr>';
+                obj += '<td>' + item.Id + '</td>';
+                obj += '<td>' + item.FirstName + '</td>';
+                obj += '<td>' + item.LastName + '</td>';
+                obj += '<td>' + item.Gender + '</td>';
+                obj += '<td>' + item.Email + '</td>';
+                obj += '<td>' + item.Mobile + '</td>';
+                obj += '<td><a href="#"class="btn btn-sm btn-primary"  data-toggle="modal" data-target="#myModal"  onclick="Edit(' + item.Id + ')">Edit</a> || <a href="#"class="btn btn-sm btn-danger" onclick="Delete(' + item.Id + ')" >Delete</a></td>';
+                obj += '</tr>';
+            });
+            $('.tbody').html(obj);
+        },
+        error: function (error) {
+            alert(error.responseText);
+        }
+    });
+}
+function Add() {
+    var obj = {
+        firstname: $('#FirstName').val(),
+        lastname: $('#LastName').val(),
+        gender: $('#Gender').val(),
+        mobile: $('#Mobile').val(),
+        email: $('#Email').val(),
+    }
+    $.ajax({
+        type: "POST",
+        url: "/Home/Create",
+        contentType: false,
+        processData: false,
+        data: JSON.stringify(obj),
+        async: false,
+        success: function () {
+            alert('Data Saved!');
+        },
+        error: function () {
+            alert("Error!");
+        }
+    });
+}
+function Delete(Id) {
+    if (confirm("Are you sure, You want to delete this record")) {
+        $.ajax({
+            url: '/Home/Delete?id=' + Id,
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json;charset=utf-8',
+            success: function () {
+                alert('Deleted Record!');
+                ShowDataAll();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+function Edit(Id) {
+    $.ajax({
+        url: '/Home/Edit?id=' + Id,
+        type: 'GET',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        data: { Id: $('#Id').val() },
+        success: function (data) {
+            if (data != null) {
+                $('#Id').val(data.Id);
+                $('#FirstName').val(data.FirstName);
+                $('#LastName').val(data.LastName);
+                $('#Gender').val(data.Gender);
+                $('#Mobile').val(data.Mobile);
+                $('#Email').val(data.Email);
+            }
+            else {
+                alert('Something Wrong!');
+            }
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+
+function Update() {
+    debugger;
+    var obj = {
+        id:$('#Id').val(),
+        firstname: $('#FirstName').val(),
+        lastname: $('#LastName').val(),
+        gender: $('#Gender').val(),
+        mobile: $('#Mobile').val(),
+        email: $('#Email').val(),
+    }
+    $.ajax({
+        type: "POST",
+        url: "/Home/Update",
+        contentType: false,
+        processData: false,
+        data: JSON.stringify(obj),
+        async: false,
+        success: function (response) {
+            $('#myModal').hide();
+            alert('Data Updated!');
+        },
+        error: function () {
+            alert("Error!");
+        }
+    });
+}
